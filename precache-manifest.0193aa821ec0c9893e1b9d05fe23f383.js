63d08b31e77e3cb273211ba3cb6dfe09572c0961
philliploy <ploy3_98@yahoo.com>
2019-09-05 09:31:33 -0500

    Updates

---
+++ precache-manifest.0193aa821ec0c9893e1b9d05fe23f383.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502e4a22b188cfe2b22b49d823d6d889",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "04c410fd9fb3a5b251f7",
+    "url": "/clickygame/static/css/main.65e7104d.chunk.css"
+  },
+  {
+    "revision": "5e83aeb0a3d3423cfe0e",
+    "url": "/clickygame/static/js/2.80f08b19.chunk.js"
+  },
+  {
+    "revision": "04c410fd9fb3a5b251f7",
+    "url": "/clickygame/static/js/main.814f02fc.chunk.js"
+  },
+  {
+    "revision": "5379ce6e23258ff101ed",
+    "url": "/clickygame/static/js/runtime~main.64b14ad1.js"
+  }
+]);
